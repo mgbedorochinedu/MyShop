0952a7906bd13566cee32e2383e8ebad0c2809f2
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-06-14 23:01:48 -1200

    Product Model and InMemory Product Repository Built

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        //Creating basic products information
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Categories { get; set; }
+        public string Image { get; set; }
+        //Create a Constructor so that everytime we create an instance pf Product, it automatically generate an ID instead from the DB
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ MyShop.DataAccess.InMemeory/ProductRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemeory
+{
+    public class ProductRepository
+    {
+        //Create an Object Cache
+        ObjectCache cache = MemoryCache.Default;
+        //Create internal list of product
+        List<Product> products;
+
+        //Create Constructor that will do a standard initializaton
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        //Create a Commit method 
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        //Below will create individual endpoints to Insert, Find, Delete, Edit and perhaps return the entire collections
+
+        //For Insert Method
+        public void Insert(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if(productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        //For Find Method
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+        //Returning a list of products
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        //For Delete Method
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+    }
+}
