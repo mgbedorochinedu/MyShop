a0f1a3730ef5694e948dafda448fae992d11b6a9
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-06-20 02:50:28 -1200

    Added Entity Framework

---
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.Models; 
+using System;
+using System.Collections.Generic;
+using System.Data.Entity; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    //DataContext class inherits from a data Entity Framework called DbContext
+    class DataContext : DbContext 
+    {
+        //We create a Constructor so that we can capture and pass in that connection string that the base class is expecting
+        public DataContext()
+            : base("DefaultConnection") 
+        {
+        }
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<ProductCategory> ProductCategories {get; set;} 
+
+    }
+}
