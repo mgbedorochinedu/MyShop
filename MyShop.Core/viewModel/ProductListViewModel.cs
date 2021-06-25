ff10d9a0cc2d07ba80c008c5ff96805b01c21d41
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-06-25 09:20:49 -1200

    Created Product Filter

---
+++ MyShop.Core/viewModel/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.viewModel
+{
+    //We passed in our Product list and Product Categories list to the viewModel
+    public class ProductListViewModel 
+    {
+        public IEnumerable<Product> Products { get; set; } 
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.viewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +21,23 @@ namespace MyShop.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category = null) 
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products; 
+            List<ProductCategory> categories = productCategories.Collection().ToList(); 
+
+            if(Category == null) 
+            {
+                products = context.Collection().ToList(); 
+            } else
+            { 
+                products = context.Collection().Where(p => p.Categories == Category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+      
+            return View(model);
         }
 
         //created our view product page
@@ -33,7 +47,6 @@ namespace MyShop.WebUI.Controllers
             if(product == null)
             {
                 return HttpNotFound();
-
             }
             else
             {
