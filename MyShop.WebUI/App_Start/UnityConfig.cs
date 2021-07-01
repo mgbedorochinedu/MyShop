1a8b7b1832d66e964059796f6760d0a07a2a7947
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-07-01 02:37:16 -1200

    Created Basket UI

--- MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemeory;
 using MyShop.DataAccess.SQL;
+using MyShop.Services;
 using System;
 
 using Unity;
@@ -47,7 +48,10 @@ namespace MyShop.WebUI
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>(); 
-            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>(); 
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>(); 
         }
     }
 }
---
+++ MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        //We use the IBasketService to expose the formatted of the IBasketService
+        IBasketService basketService;
+
+        //Constructor that allows us to inject in the BasketService
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            //We use the Index page to return our basket view which is a list of all our basket items
+            var model = basketService.GetBasketItems(this.HttpContext);
+
+            return View(model);
+        }
+
+        //Endpoint for adding to Basket
+        public ActionResult AddToBasket(string Id) //AddToBasket takes in a ProductId & pass that through to the basketService
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+            return RedirectToAction("Index"); //After adding, we redirect to Action - "Index" page
+        }
+
+        //Endpoint for Removing from Basket
+        public ActionResult RemoveFromBasket(string Id) //RemoveToBasket takes in a ProductId & pass that through to the basketService
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+            return RedirectToAction("Index"); //After removing, we redirect to Action - "Index" page
+        }
+
+        //The 'BasketSummary' endpoint will be a PartialViewResult
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+            return PartialView(basketSummary);
+        }
+    }
+}
